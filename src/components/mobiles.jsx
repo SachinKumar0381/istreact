d768497f4aa1433f32e028be0f2dcb25f6ce88ac
SachinKumar0381 <sachinkuom0591998@gmail.com>
2022-05-16 18:46:37 +0530

    first

---
+++ src/components/mobiles.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Mos=()=>{
+    return (
+        <div>
+     <h2>Mobile Operating System</h2>
+     <ul>
+         <li>Android</li>
+         <li>Blackberry</li>
+         <li>Iphone</li>
+         <li>Windows Phones</li>
+     </ul>
+     <h2>Mobile Manufacturers</h2>
+     <ul>
+         <li>Samsung</li>
+         <li>HTC</li>
+         <li>Micromax</li>
+         <li>Apple</li>
+     </ul>
+     </div>
+    );
+}
+
+export default Mos;
